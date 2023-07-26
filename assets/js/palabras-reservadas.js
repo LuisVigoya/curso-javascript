844f22a2dcd418018337598170d323dec9a49f56
Luis Vigoya <vigoya-2306@hotmail.com>
2023-07-26 18:53:00 -0500

    Palabras Reservadas

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,20 @@
+
+// No lo permite hacer
+// let const = 123; porque const es una palanra reservada
+let constante = 123; // La unica forma de que sirva
+
+
+// Alguns palabras reservadas
+
+// const
+
+// if
+
+// try
+
+// while
+// for()
+
+// enum
+
+// await
