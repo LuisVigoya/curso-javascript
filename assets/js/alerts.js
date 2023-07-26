8aaed4aaebfc415730fd7bcee64265384ca79f65
Luis Vigoya <vigoya-2306@hotmail.com>
2023-07-26 18:06:48 -0500

    Primer commit

---
+++ assets/js/alerts.js
@@ -0,0 +1,11 @@
+
+// Me arroja una ventana emergente con una informacion  no retorna
+// alert('Hola Mundo');
+
+// prompt hace una ventana emergente sirve para pedir un dato retorna un string
+// let nombre = prompt('¿Cual es tu Nombre?', 'Digite aqui');
+// console.log('***',nombre,'***');
+
+// Arroja una ventana emergente y retorna un boolean
+const seleccion = confirm('¿Esta seguro que desea continuar?'); 
+console.log(seleccion);
---
+++ assets/js/app.js
@@ -0,0 +1,50 @@
+
+//alert('Hola como estan');
+
+// console.log(console.log('Hola Mundo'));
+
+// formas de crear variables
+// let a = 10;
+// var b = 10; // Esta forma de inciar una variable no se deberia usar
+// const c = 10; // Su valor no puede cambiar
+
+// Forma 1 de declarar
+// let a = 10;
+// let b= 10;
+// let c = 10;
+// let x = a + b;
+
+// Forma 2 de declarar y que es una buena practica
+let a = 10, 
+    b = 20, 
+    c = 'Hola ',
+    d = 'Luis', 
+    x = a + b;
+
+const saludo = c + d;
+
+var miNombre = 'Luis Vigoya';
+
+// Tipos de console
+// console.log('a:', a); // 1
+// console.warn('b:', b); // 2
+// console.error('c:' ,c); // 3
+// console.info('x:', x); // 4
+
+// Forma 1 de imprimir los valores junto con el nombre de la variable
+// console.log('a:', a); 
+// console.log('b:', b); 
+// console.log('c:', c); 
+// console.log('x:', x); 
+
+// Forma 2
+// console.log('%c Mis Variables', 'color: blue; font-weight: bold'); // Agrego un poco de CSS
+// console.log({a}); 
+// console.log({b}); 
+// console.log({c}); 
+// console.log({x});
+
+// Me muestra una tabla
+//console.table({a, b, c, d, x});
+
+// console.log(saludo);
