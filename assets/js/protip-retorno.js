3cdb2f4bfd75026ac6b271065999c892fd21d3e8
Luis Vigoya <vigoya-2306@hotmail.com>
2023-08-01 11:31:36 -0500

    Continuenado con protips

--- assets/js/protip-retorno.js
+++ assets/js/protip-retorno.js
@@ -45,7 +45,7 @@ const imprimeArgunmentos2 = (...args) => {
 const [edad, vivo, casado, nombre, saludo] = imprimeArgunmentos2(24, true, false, 'Fernanda', 'Hola');
 console.log({edad, vivo, casado, nombre, saludo});
 
-// Forma para imprimir mos resultados de un objeto literal
+// Forma para imprimir mis resultados de un objeto literal
 const {apellido: nuevoApellido} = crearPersona2('Luis', 'Gomez');
 console.log({nuevoApellido});
 
@@ -55,5 +55,35 @@ function imprimeArgunmentos3() {
 };
 imprimeArgunmentos3(24, true, 'Luis');
 
+// ProTip destructuracion de objetos
+let luis = {
+    nombre: 'Luis Vigoya',
+    codeName: 'Luchito',
+    vivo: true,
+    edad: 24,
+    vehiculos: ['Yamaha FZ25', 'Chevrolet Onix Turbo RS'],
+};
 
+// Forma 1 no conozco las llaves de mi objeto literal solo sus valores
+const imprimePropiedades = ( personaje ) => {
+    console.log(personaje.nombre);
+    console.log(personaje.codeName);
+    console.log(personaje.vivo);
+    console.log(personaje.edad);
+    console.log(personaje.vehiculos);
+};
+
+// Forma 2 desestructuracion
+// Si por algun motivo un argumneto viene sin valor puedo enviarle uno por defecto y es colocando un = al lado del argumento
+// Ejemplo edad solo servira si edad llega sin un valor de lo conrario mostrara el valor asignado previamente
+const imprimePropiedades2= ( {nombre, codeName, vivo, edad = 0, vehiculos} ) => {
+    console.log({nombre});
+    console.log({codeName});
+    console.log({vivo});
+    console.log({edad});
+    console.log({vehiculos});
+};
+
+imprimePropiedades(luis);
+imprimePropiedades2(luis);
 
